8977521fc27aee810ca341f50e96ab4a1791e925
Temuujin Tsogt <temuujintsogt@Temuujins-MacBook-Pro.local>
2022-06-21 21:58:35 -0500

    [IMP] improve

--- lesson16/json-expr-exercise/json-demo-app.js
+++ lesson16/json-expr-exercise/json-demo-app.js
@@ -3,6 +3,35 @@
  */
 "use strict";
 
+// sample code
+// const employee = {
+//     "employees1 " : [{
+//             "EmpId":1,
+//             "SSN":"123-45-67",
+//             "FullName":"Usain Bolt",
+//             "DateOfBirth":"1996-12-3",
+//             "Salary":15005.95,
+//             "Email":{},
+//         },{
+//             "EmpId":2,
+//             "SSN":"123-45-68",
+//             "FullName":"Anna-Marie Rodriguez",
+//             "DateOfBirth":"2000-5-21",
+//             "Salary":4562.00,
+//             "Email":"amrod@gmail.mx"
+//         },{
+//             "EmpId":3,
+//             "SSN":"123-45-69",
+//             "FullName":"Victor Luiz Garcia",
+//             "DateOfBirth":"1998-11-7",
+//             "Salary":17750.05,
+//             "Email":{}
+//         }
+//     ]
+//     };
+// console.log(employee["employees1 "][0].Email);
+
+
 // const obj = {
 //     "window": {
 //         "title": "Sample Widget",
